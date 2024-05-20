84e5f23953ecfd3c5ddb429f44189dc375f9342d
Matheus Sakai <matheus.sakai03@gmail.com>
2024-05-20 15:00:31 -0300

    feat: adição de métodos de atualizar e deletar exemplar

--- lib/data/repositories/exemplar_repository.dart
+++ lib/data/repositories/exemplar_repository.dart
@@ -20,6 +20,21 @@ abstract class IExemplarRepository {
     double? preco,
     int? prazo,
   );
+
+  Future<void> updateExemplar(
+    String id,
+    String descricao,
+    bool negociando,
+    String livroId,
+    String userId,
+    String estadoPaginaId,
+    String estadoCapaId,
+    List<String> tiposTransacaoId,
+    double? preco,
+    int? prazo,
+  );
+
+  Future<void> deleteExemplar(String id);
 }
 
 class ExemplarRepository implements IExemplarRepository {
@@ -116,6 +131,63 @@ class ExemplarRepository implements IExemplarRepository {
 
     if (response.statusCode != 200) {
       throw Exception('Falha ao criar exemplar');
+    }
+  }
+
+  @override
+  Future<void> updateExemplar(
+    String id,
+    String descricao,
+    bool negociando,
+    String livroId,
+    String userId,
+    String estadoPaginaId,
+    String estadoCapaId,
+    List<String> tiposTransacaoId,
+    double? preco,
+    int? prazo,
+  ) async {
+    final url = Uri.parse('${dotenv.env['BASE_API_URL']}exemplar/$id');
+
+    Map<String, dynamic> body = {
+      'exe_Descricao': descricao,
+      'exe_Negociando': negociando,
+      'exe_liv_id': livroId,
+      'exe_usu_id': userId,
+      'exe_epg_id': estadoPaginaId,
+      'exe_ecp_id': estadoCapaId,
+      'exe_trs_id': tiposTransacaoId,
+    };
+    if (preco != null) {
+      body['exe_Preco'] = preco;
+    }
+    if (prazo != null) {
+      body['exe_Prazo'] = prazo;
+    }
+
+    final response = await http.put(
+      url,
+      body: json.encode(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Falha ao atualizar exemplar');
+    }
+  }
+
+  @override
+  Future<void> deleteExemplar(String id) async {
+    final url = Uri.parse('${dotenv.env['BASE_API_URL']}exemplar/$id');
+
+    final response = await http.delete(
+      url,
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Falha ao deletar exemplar');
     }
   }
 }
