216f0fd9e79485a5341fba6286d121d2dfa71ed4
Matheus Sakai <matheus.sakai03@gmail.com>
2024-05-20 15:00:01 -0300

    feat: adição de ID de capa e página

--- lib/data/models/exemplar_detalhes_model.dart
+++ lib/data/models/exemplar_detalhes_model.dart
@@ -4,6 +4,7 @@ class ExemplarDetalhesModel {
   final bool negociando;
   final String usuID;
   final String titulo;
+  final String ano;
   final String livID;
   final String isbn;
   final String autor;
@@ -11,7 +12,9 @@ class ExemplarDetalhesModel {
   final List<String> categorias;
   final String sinopse;
   final String estadoCapa;
+  final String ecpId;
   final String estadoPaginas;
+  final String epgId;
 
   ExemplarDetalhesModel({
     required this.id,
@@ -19,6 +22,7 @@ class ExemplarDetalhesModel {
     required this.negociando,
     required this.usuID,
     required this.titulo,
+    required this.ano,
     required this.livID,
     required this.isbn,
     required this.autor,
@@ -26,7 +30,9 @@ class ExemplarDetalhesModel {
     required this.categorias,
     required this.sinopse,
     required this.estadoCapa,
+    required this.ecpId,
     required this.estadoPaginas,
+    required this.epgId,
   });
 
   factory ExemplarDetalhesModel.fromJson(Map<String, dynamic> json) {
@@ -36,6 +42,7 @@ class ExemplarDetalhesModel {
       negociando: json['exe_Negociando'],
       usuID: json['exe_usu_id'],
       titulo: json['livro']['liv_Titulo'],
+      ano: json['livro']['liv_Ano'],
       livID: json['livro']['liv_Id'],
       isbn: json['livro']['liv_ISBN'],
       autor: json['livro']['autor']['aut_Nome'],
@@ -45,7 +52,9 @@ class ExemplarDetalhesModel {
       ],
       sinopse: json['livro']['liv_Sinopse'],
       estadoCapa: json['estadoCapa']['ecp_Nome'],
+      ecpId: json['estadoCapa']['ecp_Id'],
       estadoPaginas: json['estadoPaginas']['epg_Nome'],
+      epgId: json['estadoPaginas']['epg_Id'],
     );
   }
 }
