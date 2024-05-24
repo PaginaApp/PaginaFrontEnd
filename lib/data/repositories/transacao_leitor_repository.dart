50303f30d6ab31da37a2bdd7c4e7e134dd740336
Matheus Sakai <matheus.sakai03@gmail.com>
2024-05-24 17:09:49 -0300

    feat: criação da classe TransacaoLeitorRepository

---
+++ lib/data/repositories/transacao_leitor_repository.dart
@@ -0,0 +1,36 @@
+import 'package:projeto_pagina/data/models/transacao_leitor_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+abstract class ITransacaoLeitorRepository {
+  Future<List<TransacaoLeitorModel>> getTransacoesLeitorById(String id);
+}
+
+class TransacaoLeitorRepository implements ITransacaoLeitorRepository {
+  @override
+  Future<List<TransacaoLeitorModel>> getTransacoesLeitorById(String id) async {
+    final url = Uri.parse('${dotenv.env['BASE_API_URL']}transacao/leitor/$id');
+    final response = await http.get(
+      url,
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+
+      if (responseData is Map<String, dynamic>) {
+        final List<dynamic> transacoesData = responseData['results'];
+
+        final List<TransacaoLeitorModel> transacoes = transacoesData
+            .map(
+                (e) => TransacaoLeitorModel.fromJson(e as Map<String, dynamic>))
+            .toList();
+        return transacoes;
+      } else {
+        throw Exception('Formato de resposta inesperado');
+      }
+    } else {
+      throw Exception('Erro ao buscar transações');
+    }
+  }
+}
